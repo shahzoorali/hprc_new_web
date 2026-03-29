a641b56e25cb65512c29c8e05ed8ddfb45db7d83
shahzoorali <don.poky@gmail.com>
2026-03-29 19:45:27 +0530

    fix: resolve PHP 8 compatibility in dbconnect.php and ensure database selection

--- payment/dbconnect.php
+++ payment/dbconnect.php
@@ -1,16 +1,13 @@
 <?php
 
-  // Updated to point to remote DB host instead of localhost.
-  // Make sure this IP is reachable from the web server / PHP runtime.
   $dbhost = '103.21.59.27';
   $dbuser = 'hprcin_hyd';
   $dbpass = 'Hyd@hprc!12';
+  $dbname = 'hprcin_hyd';
 
-  $conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+  $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
-  if (! $conn) {
+  if (!$conn) {
     die('Could not connect: ' . mysqli_connect_error());
-  } else {
-    mysqli_select_db($conn, 'hprcin_hyd') or die(mysqli_error($conn));
   }
 ?>
