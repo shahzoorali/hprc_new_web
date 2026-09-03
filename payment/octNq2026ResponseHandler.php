9b027c81d4346052b33df899ec7a7ef1d7f46391
shahzoorali <don.poky@gmail.com>
2026-09-03 16:11:40 +0530

    payment: tag multiple horse entries as Horse 1 / Horse 2 in webhook payload and email body

--- payment/octNq2026ResponseHandler.php
+++ payment/octNq2026ResponseHandler.php
@@ -115,7 +115,18 @@ if ($order_id) {
                     $category = isset($eventMapping[$id]) ? $eventMapping[$id] : "Event #$id";
                     $readableEvents[] = $category;
                     $horses = isset($horseData[$id]) ? (is_array($horseData[$id]) ? $horseData[$id] : [$horseData[$id]]) : ["N/A"];
-                    $readableHorses[] = $category . ": (" . implode(", ", $horses) . ")";
+                    $efis = isset($efiData[$id]) ? (is_array($efiData[$id]) ? $efiData[$id] : [$efiData[$id]]) : [];
+
+                    $formattedHorses = [];
+                    $isMultiple = count($horses) > 1;
+                    foreach ($horses as $idx => $hName) {
+                        $label = $isMultiple ? ("Horse " . ($idx + 1) . ": " . $hName) : $hName;
+                        if (!empty($efis[$idx])) {
+                            $label .= " (EFI: " . $efis[$idx] . ")";
+                        }
+                        $formattedHorses[] = $label;
+                    }
+                    $readableHorses[] = $category . ": [" . implode(" | ", $formattedHorses) . "]";
                 }
 
                 $emailData = [
